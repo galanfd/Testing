8f7ceb44f59698d37eb98e115238cb34ade454b4
galanfd <galanfd@uc.cl>
2023-04-14 19:35:21 -0400

    avances instrumentor

--- T3/instrumentor/classInstrumentor.py
+++ T3/instrumentor/classInstrumentor.py
@@ -4,12 +4,66 @@ from profiler import Profiler
 
 
 class ClassInstrumentor(NodeTransformer):
-
+    #clase que implementa metodos de visita necesario para inyectar codigo usando metodos de clase ClassProfiler y recolectar informacion de las funciones que se ejecutan
     def __init__(self):
-        pass
+        super().__init__()
+        self.current_class = None
+    
+    def visit_ClassDef(self, node: ClassDef):
+        self.current_class = node.name
+        transformedNode = NodeTransformer.generic_visit(self, node)
+        self.current_class = None
+        # import_profile_injected = parse("from classInstrumentor import ClassProfiler")
+        # transformedNode.body.insert(0, import_profile_injected.body[0])
+        return transformedNode
+    
+    def visit_Module(self, node: Module):
+        transformedNode = NodeTransformer.generic_visit(self, node)
+        import_profile_injected = parse("from classInstrumentor import ClassProfiler")
+        transformedNode.body.insert(0, import_profile_injected.body[0])
+        fix_missing_locations(transformedNode)
+        return transformedNode
+
+
+    def visit_FunctionDef(self, node: FunctionDef):
+        if self.current_class != None:
+            transformedNode = NodeTransformer.generic_visit(self, node)
+            injectedCode = parse('FunctionProfiler.record(\''+
+            transformedNode.name+"'" +', '+str(transformedNode.lineno)+', '+self.current_class+')')
+        
+            if isinstance(transformedNode.body, list):
+                transformedNode.body.insert(0, injectedCode.body[0])
+            else:
+                transformedNode.body = [injectedCode.body[0], node.body]
+
+            fix_missing_locations(transformedNode)
+            
+            return transformedNode
 
 
 class ClassProfiler(Profiler):
-
+    #Clase que rastrea y reporta las clases que se ejecutan
     def __init__(self):
-        pass
+        super().__init__()
+        self.class_name = None
+        self.class_methods = []
+        self.class_methods_executed = []
+
+    @classmethod
+    def record(cls, functionName, line, class_name):
+        cls.getInstance().ins_record(functionName, line, class_name)
+
+    def ins_record(self, functionName, line, class_name):  
+        self.class_methods.append((functionName, line, class_name))
+    
+    def report_executed_methods(self):
+        #Funcion que retorna una lista con los metodos ejecutados
+        return self.class_methods
+    def report_executed_by(self, method_name):
+        #Funcion que retorna una lista con los metodos ejecutados por un metodo en particular
+        return self.class_methods_executed
+
+
+def instrument(ast):
+    visitor = ClassInstrumentor()
+    return  fix_missing_locations(visitor.visit(ast))
---
+++ T3/readme.md
@@ -0,0 +1,14 @@
+# Declaración de Tarea
+
+## Integrantes:
+Alejandro López Torres
+Felipe Galán
+
+## Tareas por integrante
+
+Se hizo la tarea en conjunto con un estilo de programación en pares, tanto en formato online como en formato presencial.
+
+Felipe Galán: Tuvo enfásis en la parte 1 de la tarea que es referente al profiler. Alejandro aporto con dudas conceptuales.
+
+Alejandro López: Tuvo enfásis en la parte 2 de la tarea respecto al CoverageTracer.
+
